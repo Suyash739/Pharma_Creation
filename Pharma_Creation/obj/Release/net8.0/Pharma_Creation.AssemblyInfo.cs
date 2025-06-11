fee1da15d0c5196d13f94ed3d67f49a67fd03ba7
Suyash739 <123507513+Suyash739@users.noreply.github.com>
2025-06-12 00:59:24 +0530

    folder
    folder

--- Pharma_Creation/obj/Release/net8.0/Pharma_Creation.AssemblyInfo.cs
+++ Pharma_Creation/obj/Release/net8.0/Pharma_Creation.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pharma_Creation")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0069d63f2b103e85eae78551217f30b7f4c7eed8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pharma_Creation")]
